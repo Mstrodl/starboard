c3e084f9dc2a1e77bd5492544ed9d87ef31e0a50
Mary Strodl <ipadlover8322@gmail.com>
2021-05-21 15:02:48 -0400

    Ignore missing reactions prop

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ app.shortcut("reload_stars", async (ctx) => {
     timestamp: resolution.messageId,
   });
 
-  const star = reactions.message.reactions.find(
+  const star = reactions.message.reactions?.find(
     (reaction) => reaction.name == "star"
   );
 
@@ -124,7 +124,7 @@ app.shortcut("reload_stars", async (ctx) => {
       channel: STARBOARD_CHANNEL,
       timestamp: postId,
     });
-    const star = channelReactions.message.reactions.find(
+    const star = channelReactions.message.reactions?.find(
       (reaction) => reaction.name == "star"
     );
     if (star) {
